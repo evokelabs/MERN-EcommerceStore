6595aec0921bf815f1a93ae8851bbd54c2580853
Adrian Pikios <adrian@evokelabs.com>
2022-06-05 09:59:03 +1000

    Express Setup

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types'
 const Rating = ({ value, text, color }) => {
   return (
     <div className='rating'>
@@ -70,9 +69,4 @@ Rating.defaultProps = {
   color: '#f8e825'
 }
 
-Rating.propTypes = {
-  value: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
-  color: PropTypes.string
-}
 export default Rating
