9fbcf2a0fbac21b33c9349f91d2dbb7f4bcdeb30
Adrian Pikios <adrian@evokelabs.com>
2022-06-11 10:23:42 +1000

    Front end user authentication and profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -20,12 +20,12 @@ const authUser = asyncHandler(async (req, res) => {
       token: generateToken(user._id)
     })
   } else {
-    res.status(400)
+    res.status(401)
     throw new Error('Invalid email or password')
   }
 })
 
-// @desc REgister a new user
+// @desc Register a new user
 // @route POST /api/users
 // @access Public
 
@@ -75,4 +75,33 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile, registerUser }
+// @desc Update user profile
+// @route PUT /api/users/profile
+// @access Private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id)
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, getUserProfile, registerUser, updateUserProfile }
