0da472207bca17d3943738cb955c434ed0aa241e
Adrian Pikios <adrian@evokelabs.com>
2022-06-14 21:57:12 +1000

    Product Reviews, Search & More

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,13 +4,17 @@ import {
   getProductById,
   deleteProduct,
   updateProduct,
-  createProduct
+  createProduct,
+  createProductReview,
+  getTopProducts
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTopProducts)
 router
   .route('/:id')
   .get(getProductById)
--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -14,14 +14,22 @@ import {
   PRODUCT_CREATE_FAIL,
   PRODUCT_UPDATE_REQUEST,
   PRODUCT_UPDATE_SUCCESS,
-  PRODUCT_UPDATE_FAIL
+  PRODUCT_UPDATE_FAIL,
+  PRODUCT_CREATE_REVIEW_REQUEST,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_CREATE_REVIEW_FAIL,
+  PRODUCT_TOP_SUCCESS,
+  PRODUCT_TOP_REQUEST,
+  PRODUCT_TOP_FAIL
 } from '../constants/productConstants'
 
-export const listProducts = () => {
+export const listProducts = (keyword = '', pageNumber = '') => {
   return async dispatch => {
     try {
       dispatch({ type: PRODUCT_LIST_REQUEST })
-      const { data } = await axios.get('/api/products')
+      const { data } = await axios.get(
+        `/api/products?keyword=${keyword}&pageNumber=${pageNumber}`
+      )
       dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data })
     } catch (error) {
       dispatch({
@@ -153,5 +161,57 @@ export const updateProduct = product => async (dispatch, getState) => {
           ? error.response.data.message
           : error.message
     })
+  }
+}
+
+export const createProductReview =
+  (productId, review) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: PRODUCT_CREATE_REVIEW_REQUEST
+      })
+
+      const {
+        userLogin: { userInfo }
+      } = getState()
+
+      const config = {
+        headers: {
+          'Content-type': 'application/json',
+          Authorization: `Bearer ${userInfo.token}`
+        }
+      }
+
+      await axios.post(`/api/products/${productId}/reviews`, review, config)
+
+      dispatch({
+        type: PRODUCT_CREATE_REVIEW_SUCCESS
+      })
+    } catch (error) {
+      dispatch({
+        type: PRODUCT_CREATE_REVIEW_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message
+      })
+    }
+  }
+
+export const listTopProducts = () => {
+  return async dispatch => {
+    try {
+      dispatch({ type: PRODUCT_TOP_REQUEST })
+      const { data } = await axios.get(`/api/products/top`)
+      dispatch({ type: PRODUCT_TOP_SUCCESS, payload: data })
+    } catch (error) {
+      dispatch({
+        type: PRODUCT_TOP_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message
+      })
+    }
   }
 }
